10c1864976a40ae4d2ff688593310d72e8b52ede
JJOELJOAQUIN <jlopez@audifarmsalud.com>
2026-05-14 10:04:11 -0300

    feat: update CORS configuration to include additional allowed origin

--- src/main/java/com/jowi/stock/auth/CorsConfig.java
+++ src/main/java/com/jowi/stock/auth/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "http://localhost:5174"
+            "http://localhost:5174",
+             "https://drapilarmurillo.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of(
